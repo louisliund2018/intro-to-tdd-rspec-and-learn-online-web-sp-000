90f3a5dcf222ef0b9d5b688980d74156a7e8993b
Louis Liu <louis.liu2013@gmail.com>
2020-03-21 23:30:59 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,4 +1,4 @@
-require_relative '../current_age_for_birth_year.rb'
+require_relative 'spec/current_age_for_birth_year.rb'
 
 describe "current_age_for_birth_year method" do
   it "returns the age of a person based on their year of birth" do
