fea441d648d99b6e78baba4472691741bc98a826
Louis Liu <louis.liu2013@gmail.com>
2020-03-21 23:55:28 +0000

    Done.

---
+++ current_age_for_birth_year.rb
@@ -0,0 +1,12 @@
+require_relative './spec/current_age_for_birth_year_spec.rb'
+
+describe "current_age_for_birth_year method" do
+  it "returns the age of a person based on their year of birth" do
+    age_of_person = current_age_for_birth_year(1984)
+
+    expect(age_of_person).to eq(19)
+  end
+end
+def current_age_for_birth_year(birth_year)
+  2003 - birth_year
+end
